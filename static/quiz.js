1ffcdd7a77cab606ec5a3c1805ebf1f6aed74844
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 12:18:31 -0400

    Rebecca - updating quiz.js

--- static/quiz.js
+++ static/quiz.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-    
+    $("#update").append('<div class="step">step 1:</div>')//change to dynamically go through steps
+    //need iterate through steps and add UI for ingredients
+    //need to add back end saving of user input, keeping track of user quiz score
 })
