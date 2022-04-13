c81d17f5f957807ca94583a56c05814a59a178b6
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 16:48:09 -0400

    Rebecca - fixing issue

--- static/view.js
+++ static/view.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    $("#update").append(drink_info.name)
     $("#update").append('<input type="button" class="make" value="Make this drink"></input>')
     $(".make").click(function() {
         let ref = '/front' + "/" + drink_info.id
