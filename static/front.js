3f9564dbe109a188f0e4775773734e587cc67e71
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 14:01:27 -0400

    Rebecca - Edited button

--- static/front.js
+++ static/front.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#update").append('<div id="tools">tools:</div>')//add tools by iterating through array stored by server
     $("#update").append('<div id="ingredients">ingredients:</div>')//add ingredients by iterating through array stored by server
     //images for all tools and ingredients are in a data set in the server and are accessible by name
-    $("#update").append('<input type="button" class="make" value="Start this recipe"></input>')
+    $("#update").append('<input type="button" class="make" value="Click to begin!"></input>')
     $(".make").click(function() {
         let ref = '/learn' + "/" + drink_info.id
         location.href = ref;
