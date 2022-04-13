91e5efd6fdf26dc76b691cc9ea9f44e376040925
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 12:17:45 -0400

    Rebecca - updating front.js

--- static/front.js
+++ static/front.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $("#update").append(drink_info.name)
-    $("#update").append('<div id="tools">tools</div>')//add tools
-    $("#update").append('<div id="ingredients">ingredients</div>')//add ingredients
-    $("#update").append('<input type="button" class="make" value="Make this drink"></input>')
+    $("#update").append('<div id="tools">tools:</div>')//add tools by iterating through array stored by server
+    $("#update").append('<div id="ingredients">ingredients:</div>')//add ingredients by iterating through array stored by server
+    //images for all tools and ingredients are in a data set in the server and are accessible by name
+    $("#update").append('<input type="button" class="make" value="Start this recipe"></input>')
     $(".make").click(function() {
         let ref = '/learn' + "/" + drink_info.id
         location.href = ref;
