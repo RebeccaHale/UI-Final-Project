ee4eddf3b0956845c7c8e4f8f7503aadc5c6c94d
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 12:20:34 -0400

    Update quiz.js

--- static/static/quiz.js
+++ static/static/quiz.js
@@ -1,5 +1,1 @@
-$(document).ready(function(){
-    $("#update").append('<div class="step">step 1:</div>')//change to dynamically go through steps
-    //need iterate through steps and add UI for ingredients
-    //need to add back end saving of user input, keeping track of user quiz score
-})
+//ignore this file, accidental upload, real file in previous folder
