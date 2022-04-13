27ca631d55c97b31db80f0dc67b19ea8c7434055
Rebecca Hale <rkh2128@columbia.edu>
2022-04-13 12:18:09 -0400

    Rebecca - updating learn.js

--- static/learn.js
+++ static/learn.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  
+    $("#update").append('<div class="step">step 1:</div>')//change to dynamically go through steps
+    //need iterate through steps and add UI for ingredients
+    //need to add back end saving of user input
+    //remember to transition to the quiz once the user finishes the recipe
 })
