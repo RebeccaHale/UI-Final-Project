2d9518cae1e07148f16fbf693b279e546e4dcfa6
Shawn Lou <sl4475@columbia.edu>
2022-04-16 06:20:37 -0400

    added basic quiz functionality(button clicks responsive and progress to next instruction, scoring system in place, gives feedback on answer) doesnt do anything when it reaches end of quiz yet.

--- static/quiz.js
+++ static/quiz.js
@@ -1,4 +1,47 @@
+let num_correct = 0
+let current_step = 0
 $(document).ready(function(){
     //need iterate through steps and add UI for ingredients
     //need to add back end saving of user input, keeping track of user quiz score
+    
+    for(x of drink_info.ingredients){
+        element = document.getElementById(x)
+        element.onclick = function(){checkAnswer(this)};
+        
+    }
+
+    for(x of drink_info.tools){
+        element = document.getElementById(x)
+        element.onclick = function(){checkAnswer(this)};
+    }
 })
+
+
+function checkAnswer(button){
+    //console.log(x.value)
+    choice = button.value
+    $("#chosen_step").empty()
+    if(drink_info.directions[current_step] == drink_info.match[choice]){
+        //corect answer
+        num_correct = num_correct+1
+        $("#chosen_step").text("Correct! You chose: "+ choice)
+	
+    }   
+    else{
+        //wrong answer
+        console.log("wrong answer")
+        $("#chosen_step").text("Incorrect! You chose:" + choice  + ". Correct answer is:" + drink_info.directions[current_step])
+	
+
+    }
+    $("#current_score").text(num_correct+ "/" + drink_info.number_steps)
+	current_step = current_step+1
+    if(current_step == drink_info.number_steps){
+        //end of quiz
+        //do something else
+    }
+	console.log(drink_info.directions[x])
+
+    
+
+}
