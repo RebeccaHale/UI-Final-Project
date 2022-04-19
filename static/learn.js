1eb119317090daea811cc1c2b69fa9e38d9a21b6
Rebecca Hale <rkh2128@columbia.edu>
2022-04-18 22:20:24 -0400

    updated UI

--- static/learn.js
+++ static/learn.js
@@ -1,91 +1,99 @@
-let x=0; 
+let current_click = ""
+let current_drag = ""
+/*function run(current_step, current_num){
+    console.log("step = " + current_num + ": " + current_step)
+    let isTool = 0
+    if(current_step.includes("Ingredients")==true){
+        isTool++
+    }
+    if(isTool){
+        //fix later 
+        return 1
+    }
+    else{
+        //check draggable
+        current_ingredient = current_step.substring(4)
+        if(current_click==current_ingredient){
+            return 1
+        }
+        return 0
+    }
+}*/
 
-// store user data
-let user_choices = {}
-
-// just to check if a choice has already been logged
-let madeChoice = false
-
-function displaythisstep(){
-	$("#step_instructions").empty()
-	var new_box=$("<div>")
-	$("#step_instructions").text("Your next instruction is: "+drink_info.directions[x])
-	console.log(drink_info.directions[x])
-
-	//$(new_box).text(username+" "+tweet)
-	//$("#listy").prepend(new_box)
+function update_step(){
+    step = step + 1
+    if(step==drink_info.number_steps){//finished the recipe
+        step=0
+        $.ajax({
+            type: "POST",
+            url: "../update_step",                
+            dataType : "json",
+            contentType: "application/json; charset=utf-8",
+            data : JSON.stringify(step),
+            success: function(result){
+                $("#step_label").text("Congratulations! You finished your recipe. ")
+                let ref = "window.location.href=" + "'/quiz/"+ drink_info.id + "';";
+                $("#step_label").append('<input type="button" id="quiz_button" value="Quiz Me!" onclick="' + ref + '"></input>')
+            },
+            error: function(request, status, error){
+                console.log("Error");
+                console.log(request)
+                console.log(status)
+                console.log(error)
+            }
+        });
+    }
+    else{
+        $.ajax({
+            type: "POST",
+            url: "../update_step",                
+            dataType : "json",
+            contentType: "application/json; charset=utf-8",
+            data : JSON.stringify(step),
+            success: function(result){
+                //check that step in server is updating
+                $("#step_label").text("Step: " + drink_info.directions[step])
+            },
+            error: function(request, status, error){
+                console.log("Error");
+                console.log(request)
+                console.log(status)
+                console.log(error)
+            }
+        });
+    }
+    
 }
 
-
 $(document).ready(function(){
-   displaythisstep()
-   $(".ingredient_button").click(function(){
+    $("#step_label").text("Step: " + drink_info.directions[step])
+    $(".ingredient_label").draggable({
+        revert: "invalid",
+        start: function(e, ui) {
+            current_drag = $(this).text()
+        }
+        
+    });
+    $("#cup").droppable({
+        drop: function(event, ui) {
+            current_click = current_drag
+            current_drag = ""
+            if(current_click==drink_info.directions[step]){
+                update_step()
+                console.log("Step completed! new step: " + drink_info.directions[step])
+            }
+        }
+    })
+    /*$.each(steps, function( value ) {
+        console.log(  steps[value] );
+    });*/
 
-		// log user choice
-	   if (!madeChoice) {
-			console.log("add choice")
+    $(".tool_button").click(function(){
+        current_click = $(this).val()
+        if(current_click==drink_info.directions[step]){
+            update_step()
+        }
+    })
+    
 
-			choice = this.value;
-			user_choices[x + 1] = drink_info.match[choice];
-
-			madeChoice = true;
-	   }
-
-	   alert("Step completed, proceed to next step.")
-   })
-   $("#learn_next_step_button").click(function(){
-
-	if(x<parseInt(drink_info.number_steps)-1){
-		
-		x=x+1
-		displaythisstep()
-		console.log(x)
-
-		// reset madeChoice
-		madeChoice = false
-	}
-	//else if(x==drink_info.number_steps-1){
-		
-	//}
-	else{
-		// get date + time
-		let dt = new Date();
-		let time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-
-		// make new entry for backend
-		let new_entry = {
-			time: time,
-			user_choices: user_choices,
-			name: drink_info.name
-		}
-
-		// ajax post
-		$.ajax({
-			type: "POST",
-			url: "add",                
-			dataType : "json",
-			contentType: "application/json; charset=utf-8",
-			data : JSON.stringify(new_entry),
-			success: function(result){
-				console.log(result);
-				console.log(result["user_choices"])
-			},
-			error: function(request, status, error){
-				console.log("Error");
-				console.log(request)
-				console.log(status)
-				console.log(error)
-			}
-		});
-		$("#learn_next_step_button").html("Go To Quiz")
-		window.location.href = "/quiz/"+drink_info.id;
-
-	}
-
-
-
-   })
-    //change to dynamically go through steps
-    //need iterate through steps and add UI for ingredients
-    //remember to transition to the quiz once the user finishes the recipe
 })
