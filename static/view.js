50eb04d6fa070f773d7ece56d16a6a2d4a9b3bc9
sl4475 <sl4475@columbia.edu>
2022-04-25 14:48:59 -0400

    changed button width to match picture

--- static/view.js
+++ static/view.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#update").append('<input type="button" class="make" value="Make this drink"></input>')
+    $("#update").append('<input type="button" class="make"  style = "width: 150px" value="Make this drink"></input>')
     $(".make").click(function() {
         let ref = '/front' + "/" + drink_info.id
         location.href = ref;
