7e04677c2b8c6a8bec92a307f4f0feeea70b0b13
Rebecca Hale <rkh2128@columbia.edu>
2022-04-25 21:27:12 -0400

    fixing view UI

--- static/view.js
+++ static/view.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-    $("#update").append('<input type="button" class="make"  style = "width: 150px" value="Make this drink"></input>')
+    $("#update2").append('<br>')
+    $("#update2").append('<br>')
+    $("#update2").append('<input type="button" class="make"  style = "width: 150px" value="Learn Recipe"></input>')
+    $("#update2").append('<br>')
+    $("#update2").append('<br>')
+    $("#update2").append('<input type="button" class="make2"  style = "width: 150px" value="Recipe Quiz"></input>')
+    
     $(".make").click(function() {
         let ref = '/front' + "/" + drink_info.id
         location.href = ref;
     })
+    $(".make2").click(function() {
+        let ref = '/quiz' + "/" + drink_info.id
+        location.href = ref;
+    })
 })
+
