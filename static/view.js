be0e66afb7f56b7725a111660167ced495632d26
Jonathan <jec2274@columbia.edu>
2022-04-27 15:46:30 -0400

    Minor stylistic things in both the code and the viewables.

--- static/view.js
+++ static/view.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     $("#update2").append('<br>')
     $("#update2").append('<br>')
-    $("#update2").append('<input type="button" class="make"  style = "width: 150px" value="Learn Recipe"></input>')
+    $("#update2").append('<input type="button" class="make buttoncolors LearnRecipe"  style = "width: 150px" value="Learn Recipe"></input>')
     $("#update2").append('<br>')
     $("#update2").append('<br>')
-    $("#update2").append('<input type="button" class="make2"  style = "width: 150px" value="Recipe Quiz"></input>')
-    
-    $(".make").click(function() {
+    $("#update2").append('<input type="button" class="make buttoncolors RecipeQuiz"  style = "width: 150px" value="Recipe Quiz"></input>')
+
+    $(".LearnRecipe").click(function() {
         let ref = '/front' + "/" + drink_info.id
         location.href = ref;
     })
-    $(".make2").click(function() {
+    $(".RecipeQuiz").click(function() {
         let ref = '/quiz' + "/" + drink_info.id
         location.href = ref;
     })
 })
-
